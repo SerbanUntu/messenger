3b54af6378a17bfcc0ee95f6d9b0ad66a520a6f4
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-01-31 20:18:06 +0100

    init: setup react, ts, webpack
    Set up React project with TypeScript and Webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: './index.js',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(js|jsx|ts|tsx)$/i,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+					},
+				},
+			},
+		],
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			// favicon: './favicon.ico',
+		}),
+	],
+}
